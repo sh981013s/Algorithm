cf583fc59a3cb383f212f1632d149dc199f1dd57
sh981013s <sh981013s@gmail.com>
2021-07-27 23:36:38 +0900

    Create 5338.js

---
+++ backjoon/solved.ac/bronze5/5338.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let answer = '';
+
+answer += `       _.-;;-._\n`;
+answer += `'-..-'|   ||   |\n`;
+answer += `'-..-'|_.-;;-._|\n`;
+answer += `'-..-'|   ||   |\n`; 
+answer += `'-..-'|_.-''-._|`;
+
+console.log(answer);
