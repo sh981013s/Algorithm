394775565d2f2129d569a1b311da420b1fbf56ce
sh981013s <sh981013s@gmail.com>
2021-07-28 00:19:19 +0900

    Create 5554.js

---
+++ backjoon/solved.ac/bronze5/5554.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const fs = require('fs');
+const input = fs.readFileSync('input.txt').toString().trim().split('\n').map(Number);
+
+
+function answer() {
+    let totalTime = 0;
+
+    for(let i=0;i<input.length;i++) {
+        totalTime += input[i];
+    };
+
+    const min = Math.floor(totalTime / 60);
+    const sec = totalTime - (min * 60);
+
+    console.log(min);
+    console.log(sec);
+};
+
+answer();
