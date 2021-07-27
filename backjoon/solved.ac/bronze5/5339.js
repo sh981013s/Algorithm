1d8213f04841ee85df867e14b675e0c947432219
sh981013s <sh981013s@gmail.com>
2021-07-27 23:42:40 +0900

    Create 5339.js

---
+++ backjoon/solved.ac/bronze5/5339.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let answer = '';
+
+answer += `     /~\\\n`;
+answer += `    ( oo|\n`;
+answer += `    _\\=/_\n`;
+answer += `   /  _  \\\n`;
+answer += `  //|/.\\|\\\\\n`;
+answer += ` ||  \\ /  ||\n`;
+answer += `============\n`;
+answer += `|          |\n`;
+answer += `|          |\n`;
+answer += `|          |\n`;
+
+console.log(answer);
