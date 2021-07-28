4a56c7b8a101ee3a11edfc48da0dbbbe60190df0
sh981013s <sh981013s@gmail.com>
2021-07-28 16:06:27 +0900

    Create 10170.js

---
+++ backjoon/solved.ac/bronze5/10170.js
@@ -0,0 +1,17 @@
+let answer = '';
+
+answer += `NFC West       W   L  T\n`;
+answer += `-----------------------\n`;
+answer += `Seattle        13  3  0\n`;
+answer += `San Francisco  12  4  0\n`;
+answer += `Arizona        10  6  0\n`;
+answer += `St. Louis      7   9  0\n`;
+answer += `\n`;
+answer += `NFC North      W   L  T\n`;
+answer += `-----------------------\n`;
+answer += `Green Bay      8   7  1\n`;
+answer += `Chicago        8   8  0\n`;
+answer += `Detroit        7   9  0\n`;
+answer += `Minnesota      5  10  1\n`;
+
+console.log(answer);
