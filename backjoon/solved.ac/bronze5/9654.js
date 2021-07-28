c0d2d09739c9cb600640657cc2d1c4aca1b6d65b
sh981013s <sh981013s@gmail.com>
2021-07-28 12:58:53 +0900

    Create 9654.js

---
+++ backjoon/solved.ac/bronze5/9654.js
@@ -0,0 +1,10 @@
+let answer = '';
+
+answer += `SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n`;
+answer += `N2 Bomber      Heavy Fighter  Limited    21        \n`;
+answer += `J-Type 327     Light Combat   Unlimited  1         \n`;
+answer += `NX Cruiser     Medium Fighter Limited    18        \n`;
+answer += `N1 Starfighter Medium Fighter Unlimited  25        \n`;
+answer += `Royal Cruiser  Light Combat   Limited    4         `;
+
+console.log(answer);
