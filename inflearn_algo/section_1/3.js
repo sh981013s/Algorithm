cc6982fc141f66c262dcf1a58995c34bc91aa4e3
SeungHwan-Lee <sh981013s@gmail.com>
2021-08-24 16:25:03 +0900

    3.js

---
+++ inflearn_algo/section_1/3.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const fs = require('fs');
+const input = fs.readFileSync('input.txt').toString().trim().split('\n').map(Number);
+
+const studentNum = input[0];
+
+let das = Math.floor(studentNum / 12);
+
+if (studentNum % 12 !== 0) {
+    das += 1;
+}
+
+console.log(das);
