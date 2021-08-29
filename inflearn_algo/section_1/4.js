3c35e2ea760b75e08537c93a543e9d2b05c9971f
SeungHwan-Lee <sh981013s@gmail.com>
2021-08-29 09:50:09 +0900

    4.js

---
+++ inflearn_algo/section_1/4.js
@@ -0,0 +1,16 @@
+"use strict";
+
+(function() {
+
+    const fs = require('fs');
+    const input = fs.readFileSync('input.txt').toString().trim().split('\n').map(Number);
+
+    let sum = 0;
+
+    for (let i = 0; i <= input[0]; i++) {
+        sum += i;
+    }
+
+    console.log(sum);
+})();
+
