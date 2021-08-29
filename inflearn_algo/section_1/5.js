1c5111f0eea8057d6a822c3ae9bdea2cfe55bd8c
SeungHwan-Lee <sh981013s@gmail.com>
2021-08-29 10:06:29 +0900

    5.js

--- inflearn_algo/section_1/5.js
+++ inflearn_algo/section_1/5.js
@@ -7,11 +7,13 @@
 
     let small = input[0]
 
-    for (let i = 1; i < input.length; i++) {
-        if (small > input[i]) {
-            small = input[i];
-        }
-    }
+    // for (let i = 1; i < input.length; i++) {
+    //     if (small > input[i]) {
+    //         small = input[i];
+    //     }
+    // }
+
+    small = Math.min(...input);
 
     console.log(small);
 })();
