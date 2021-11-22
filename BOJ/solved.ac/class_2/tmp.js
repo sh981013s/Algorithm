62ec9b804b6b32041d14aed5ac406e521059f4e2
SeungHwan-Lee <sh981013s@gmail.com>
2021-11-22 21:00:12 +0900

    boj 1654

--- BOJ/solved.ac/class_2/tmp.js
+++ BOJ/solved.ac/class_2/tmp.js
@@ -1,24 +1,26 @@
-'use strict';
-
 (() => {
-	const fs = require('fs');
-	const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+	const a = `4 11
+802
+743
+457
+539`;
 
+	const input = a.toString().trim().split('\n');
 	const [n, k] = input
 		.shift()
 		.split(' ')
 		.map((a) => +a);
 	const lines = input.map((a) => +a).sort();
 
-	let max = Math.max(...lines);
 	let min = 1;
+	let max = Math.max(...lines);
 
 	while (min <= max) {
-		let mid = parseInt((max + min) / 2);
-		let howManyPieces = lines
+		let mid = parseInt((min + max) / 2);
+		let num = lines
 			.map((line) => parseInt(line / mid))
 			.reduce((a, b) => a + b, 0);
-		if (howManyPieces >= k) {
+		if (num >= k) {
 			min = mid + 1;
 		} else {
 			max = mid - 1;
