e79acb1e7e0dbc82c5665d010ddd2a172464fc5d
SeungHwan-Lee <sh981013s@gmail.com>
2022-03-17 14:28:40 +0900

    feat: Extract the domain name from a URL

---
+++ codewars/5kyu/Extract the domain name from a URL.js
@@ -0,0 +1,9 @@
+function domainName(url) {
+  url = url.replace("https://", "");
+  url = url.replace("http://", "");
+  url = url.replace("www.", "");
+
+  return url.split(".")[0];
+}
+
+console.log(domainName("http://google.com"));
