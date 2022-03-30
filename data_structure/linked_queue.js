b118e31cb0947d84a4bfacb7b94d7a46365a5ad3
SeungHwan-Lee <sh981013s@gmail.com>
2022-03-30 15:04:48 +0900

    queue

---
+++ data_structure/linked_queue.js
@@ -0,0 +1,30 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  enqueue(val) {
+    const newNode = new Node(val);
+    if (this.head === null) {
+      this.head = this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.size++;
+  }
+
+  dequeue() {
+    this.head = this.head.next;
+    this.size--;
+  }
+}
