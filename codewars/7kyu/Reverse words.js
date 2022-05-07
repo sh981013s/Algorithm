183fa9085ca003e02044433add3795a428ad964b
SeungHwan-Lee <sh981013s@gmail.com>
2022-05-07 19:57:10 +0900

    Reverse words

---
+++ codewars/7kyu/Reverse words.js
@@ -0,0 +1,8 @@
+function reverseWords(str) {
+  return str
+    .split(" ")
+    .map((word) => word.split("").reverse().join(""))
+    .join(" ");
+}
+
+console.log(reverseWords("This is an example!"));
