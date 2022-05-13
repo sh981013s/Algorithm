c5c6289c01294767688c21ad29e4a9893382daa7
SeungHwan-Lee <sh981013s@gmail.com>
2022-05-14 00:48:35 +0900

    10156

---
+++ BOJ/solved.ac/bronze4/10156.js
@@ -0,0 +1,15 @@
+"use strict";
+
+(() => {
+  const fs = require("fs");
+  const input = fs
+    .readFileSync("/dev/stdin")
+    .toString()
+    .trim()
+    .split(" ")
+    .map(Number);
+
+  console.log(
+    input[0] * input[1] - input[2] <= 0 ? 0 : input[0] * input[1] - input[2]
+  );
+})();
