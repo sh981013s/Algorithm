096aa0c58f606322b14de3eb39fa4909c99e72ca
SeungHwan-Lee <sh981013s@gmail.com>
2022-05-16 23:37:27 +0900

    15873

--- BOJ/solved.ac/bronze4/14623.js
+++ BOJ/solved.ac/bronze4/14623.js
@@ -5,6 +5,7 @@
   const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
   const first = parseInt(input[0], 2);
+
   const second = parseInt(input[1], 2);
 
   console.log((first * second).toString(2));
