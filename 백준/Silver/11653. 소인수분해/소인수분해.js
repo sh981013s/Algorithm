7f87bb2c6dba93818fa657cd529901d71036d155
SeungHwan(Jason) Lee <81363031+sh981013s@users.noreply.github.com>
2022-06-27 21:19:24 +0900

    [Silver V] Title: 소인수분해, Time: 252 ms, Memory: 9888 KB -BaekjoonHub

---
+++ 백준/Silver/11653. 소인수분해/소인수분해.js
@@ -0,0 +1,15 @@
+"use strict";
+
+(() => {
+  const fs = require("fs");
+  let input = +fs.readFileSync("/dev/stdin").toString().trim();
+
+  let result = [];
+  for (let i = 2; i <= input; i++) {
+    while (input % i === 0) {
+      input = input / i;
+      result.push(i);
+    }
+  }
+  console.log(result.join("\n"));
+})();
